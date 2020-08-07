c0a2c65874fcbd1cbee45116830c5bc93e9511c6
softchris <noringc@gmail.com>
2020-08-07 16:55:07 +0100

    performance improvement by a looot

--- src/game.ts
+++ src/game.ts
@@ -61,21 +61,29 @@ class Game {
   private WIDTH = 1200;
   // private HEIGHT = 768;
   private tiles: Array<{
-    x: number,
-    y: number,
-    terrain: string,
-    sprite: HTMLImageElement,
-    text: string
+    x: number;
+    y: number;
+    terrain: string;
+    sprite: HTMLImageElement;
+    text: string;
   }>;
-  private canvas:  null | HTMLCanvasElement;
-  private gameCursor: { x: number, y: number };
+  private gridCanvas: null | HTMLCanvasElement;
+  private gridGraphics: null | CanvasRenderingContext2D;
+  private canvas: null | HTMLCanvasElement;
+  private bgCanvas: null | HTMLCanvasElement;
+  private bgGraphics: null | CanvasRenderingContext2D;
+  private gameCursor: { x: number; y: number };
   // private selected: Array<Honeycomb.Hex<{ size: number }>>;
   private selectedSprite: TerrainType;
   private textureFactory: TextureFactory;
   private fogOfWar = false;
 
   constructor() {
+    this.gridCanvas = <HTMLCanvasElement>document.getElementById('grid');
+    this.gridGraphics = this.gridCanvas.getContext("2d", { alpha: false });
     const canvasEl = document.getElementById("canvas");
+    this.bgCanvas = <HTMLCanvasElement>document.getElementById("background");
+    this.bgGraphics = this.bgCanvas.getContext("2d");
     this.canvas = <HTMLCanvasElement>canvasEl;
     this.graphics = this.canvas?.getContext("2d");
     this.textureFactory = new TextureFactory();
@@ -87,20 +95,22 @@ class Game {
     this.grid = this.Grid.rectangle({ width: this.COLUMNS, height: this.ROWS });
     this.tiles = [];
 
-    emitter.on('selected', this.handleBoardClick.bind(this))
-    emitter.on('keyup', this.handleKeyPress.bind(this))
-    emitter.on('terrainSelection', this.handleTerrainSelection.bind(this));
-    emitter.on('saveLevel', this.handleSaveLevel.bind(this));
-    emitter.on('loadLevel', this.handleLoadLevel.bind(this));
-    emitter.on('fogChecked', this.handleFogChecked.bind(this));
+    emitter.on("selected", this.handleBoardClick.bind(this));
+    emitter.on("keyup", this.handleKeyPress.bind(this));
+    emitter.on("terrainSelection", this.handleTerrainSelection.bind(this));
+    emitter.on("saveLevel", this.handleSaveLevel.bind(this));
+    emitter.on("loadLevel", this.handleLoadLevel.bind(this));
+    emitter.on("fogChecked", this.handleFogChecked.bind(this));
   }
   start() {
     this.textureFactory.load().then(() => {
       setupEventHandlers();
       log("Creating game tiles");
       log("Drawing grid");
+      this.drawAddedTiles();
       this.drawGrid();
-    })
+      this.drawCursorAndFog();
+    });
   }
   handleBoardClick(point) {
     const { x, y } = point;
@@ -112,18 +122,17 @@ class Game {
       this.tiles.push({
         x: hex.x,
         y: hex.y,
-        text: '',
+        text: "",
         terrain: this.selectedSprite,
-        sprite: this.textureFactory.geTextureByName(this.selectedSprite)
+        sprite: this.textureFactory.geTextureByName(this.selectedSprite),
       });
-      
     } else {
       this.tiles = this.tiles.filter((t) => {
         return !(t.x === hex.x && t.y === hex.y);
-      })
+      });
     }
 
-    this.drawGrid();
+    this.drawAddedTiles();
   }
   handleKeyPress(evt) {
     console.log(evt.key);
@@ -132,44 +141,51 @@ class Game {
     if (evt.key === "ArrowUp") {
       this.gameCursor.y = this.gameCursor.y - 1 < 0 ? 0 : this.gameCursor.y - 1;
     } else if (evt.key === "ArrowDown") {
-      this.gameCursor.y = this.gameCursor.y + 1 > this.ROWS - 1 ? this.ROWS - 1 : this.gameCursor.y + 1;
+      this.gameCursor.y =
+        this.gameCursor.y + 1 > this.ROWS - 1
+          ? this.ROWS - 1
+          : this.gameCursor.y + 1;
     } else if (evt.key === "ArrowLeft") {
       this.gameCursor.x = this.gameCursor.x - 1 < 0 ? 0 : this.gameCursor.x - 1;
     } else if (evt.key === "ArrowRight") {
       this.gameCursor.x =
-        this.gameCursor.x + 1 > this.COLUMNS - 1 ? this.COLUMNS - 1 : this.gameCursor.x + 1;
+        this.gameCursor.x + 1 > this.COLUMNS - 1
+          ? this.COLUMNS - 1
+          : this.gameCursor.x + 1;
     }
 
     const gameTile = getInfoTileFromHex(this.gameCursor, this.tiles);
-    
+
     if (gameTile) {
       toolTip(`Terrain tile of type ${gameTile.terrain}`);
     } else {
-      toolTip('No tile here');
+      toolTip("No tile here");
     }
-    
 
-    this.drawGrid();
+    this.drawCursorAndFog();
   }
   handleTerrainSelection(evt) {
     this.selectedSprite = evt.target.value;
   }
   handleSaveLevel() {
-    let filename = prompt('Enter file name (.json is appended)')
+    let filename = prompt("Enter file name (.json is appended)");
     if (filename) {
-      let element = document.createElement('a');
-      let json = this.tiles.map(tile => {
+      let element = document.createElement("a");
+      let json = this.tiles.map((tile) => {
         return {
           x: tile.x,
           y: tile.y,
           terrain: tile.terrain,
-          text: tile.text
-        }
-      })
+          text: tile.text,
+        };
+      });
 
-      element.setAttribute('href', 'data:application/json; charset=utf-8,' + JSON.stringify(json));
-      element.setAttribute('download', filename);
-      element.style.display = 'none';
+      element.setAttribute(
+        "href",
+        "data:application/json; charset=utf-8," + JSON.stringify(json)
+      );
+      element.setAttribute("download", filename);
+      element.style.display = "none";
       document.body.appendChild(element);
       element.click();
       document.body.removeChild(element);
@@ -177,84 +193,105 @@ class Game {
   }
   handleLoadLevel(file) {
     let reader = new FileReader();
-    reader.readAsText(file, 'UTF-8');
+    reader.readAsText(file, "UTF-8");
 
     // here we tell the reader what to do when it's done reading...
-    reader.onload = readerEvent => {
+    reader.onload = (readerEvent) => {
       if (readerEvent) {
         let content = readerEvent.target?.result;
-        let json = JSON.parse('' + content);
+        let json = JSON.parse("" + content);
         // clean up
-        
+
         // reassign existing tiles
-        this.tiles = json.map(item => {
+        this.tiles = json.map((item) => {
           return {
             x: item.x,
             y: item.y,
             text: item.text,
             terrain: item.terrain,
-            sprite: this.textureFactory.geTextureByName(item.terrain)
-          }
-        })
+            sprite: this.textureFactory.geTextureByName(item.terrain),
+          };
+        });
         // redraw
-        this.drawGrid();
+        this.drawAddedTiles();
+        this.drawCursorAndFog();
       }
-    }
+    };
   }
   handleFogChecked(evt) {
-     this.fogOfWar = evt.target.checked;
-     this.drawGrid(); 
+    this.fogOfWar = evt.target.checked;
+    this.drawCursorAndFog();
   }
   drawAddedTiles() {
-    this.tiles.forEach(tile => {
-      const { x, y } = tile;
-      const hex = this.grid.get({ x, y });
-      if (hex) {
-        const point = hex?.toPoint();
-        this.graphics?.drawImage(tile.sprite, point.x, point.y);
-      }
-    })
+    if (this.bgCanvas && this.bgGraphics) {
+      this.bgGraphics.clearRect(
+        0,
+        0,
+        this.bgCanvas.width,
+        this.bgCanvas.height
+      );
+      this.tiles.forEach((tile) => {
+        const { x, y } = tile;
+        const hex = this.grid.get({ x, y });
+        if (hex) {
+          const point = hex?.toPoint();
+          this.bgGraphics?.drawImage(tile.sprite, point.x, point.y);
+        }
+      });
+    }
   }
-  drawGrid() {
+
+  drawCursorAndFog() {
     if (this.canvas && this.graphics) {
       this.graphics.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      this.graphics.strokeStyle = 'black';
-      //this.app.stage.removeChild(this.richText);
-      // this.graphics.lineStyle(1, 0x999999);
-      this.drawAddedTiles();
+      const cursorNeighbourList = [this.gameCursor, ...this.getNeighbours()];
       this.grid.forEach((hex) => {
         if (this.graphics && this.canvas) {
           const point = hex.toPoint();
-          // add the hex's position to each of its corner points
           const corners = hex.corners().map((corner) => corner.add(point));
-          // separate the first from the other corners
           const [firstCorner, ...otherCorners] = corners;
           const isCursor = isGameCursor(hex, this.gameCursor);
-          const isCursorOrNeighbour = isInList(hex, [
-            this.gameCursor,
-            ...this.getNeighbours()]
-          );
-          this.graphics.beginPath();
-          this.graphics.moveTo(firstCorner.x, firstCorner.y);
-          otherCorners.forEach(({ x, y }) => this.graphics?.lineTo(x, y));
-          this.graphics.lineTo(firstCorner.x, firstCorner.y);
-          if (isCursor) {
-            this.graphics.fillStyle = 'purple';
+          const isCursorOrNeighbour = isInList(hex, cursorNeighbourList);
+          if ((this.fogOfWar && !isCursorOrNeighbour) || isCursor) {
+            this.graphics.beginPath();
+            this.graphics.moveTo(firstCorner.x, firstCorner.y);
+            otherCorners.forEach(({ x, y }) => this.graphics?.lineTo(x, y));
+            this.graphics.lineTo(firstCorner.x, firstCorner.y);
+            this.graphics.fillStyle = isCursor ? "purple" : "black";
             this.graphics.fill();
-          } else if (!isCursorOrNeighbour && this.fogOfWar) {
-            this.graphics.fillStyle = 'black';
-            this.graphics.fill();
-          } else {
-            this.graphics.strokeStyle = 'grey';
-            this.graphics.stroke();
           }
         }
-    });
+      });
     }
   }
- 
+
+  drawGrid() {
+    if (this.gridCanvas && this.gridGraphics) {
+      this.gridGraphics.clearRect(
+        0,
+        0,
+        this.gridCanvas.width,
+        this.gridCanvas.height
+      );
+      this.gridGraphics.strokeStyle = "black";
+      this.grid.forEach((hex) => {
+        if (this.gridGraphics && this.gridCanvas) {
+          const point = hex.toPoint();
+          const corners = hex.corners().map((corner) => corner.add(point));
+          const [firstCorner, ...otherCorners] = corners;
+          this.gridGraphics.beginPath();
+          this.gridGraphics.moveTo(firstCorner.x, firstCorner.y);
+          otherCorners.forEach(({ x, y }) => this.gridGraphics?.lineTo(x, y));
+          this.gridGraphics.lineTo(firstCorner.x, firstCorner.y);
+          this.gridGraphics.strokeStyle = "grey";
+          this.gridGraphics.stroke();
+        }
+      });
+    }
+  }
+
   private getNeighbours(): Honeycomb.Hex<{}>[] {
-    const Hex = Honeycomb.extendHex({ orientation: 'pointy' });
+    const Hex = Honeycomb.extendHex({ orientation: "pointy" });
     return this.grid.hexesInRange(Hex(this.gameCursor.x, this.gameCursor.y), 2);
   }
 }
